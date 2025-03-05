be75115b5efc73da4251aee10c15f8c28559640a
ALulin1 <linnluu1@gmail.com>
2025-03-05 12:59:31 +0300

    Descriptive_norm_perception.py

--- Descriptive_norm_perception.py
+++ Descriptive_norm_perception.py
@@ -1,4 +1,11 @@
-#%%
+# Patent Pending: [RU2024170315-001D] - Dynamic Norm Induction in Multi-Agent Healthcare Systems
+# Use of this technology requires explicit authorization from patent holders.
+# Copyright [2024] [作者/机构名]
+# Licensed under the Apache License, Version 2.0 (the "License");
+# Medical use requires compliance with ethical guidelines in ETHICAL_GUIDELINES.md.
+#
+
+%%
 import random
 import numpy as np
 from scipy.stats import norm
